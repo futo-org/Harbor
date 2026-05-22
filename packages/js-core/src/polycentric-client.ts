44eabdb1d020ff4c54785fbf33a0348aa4749f07
Mark Harding <mark@futo.org>
2026-05-22 11:13:45 +0100

    (fix): js build from linter

--- packages/js-core/src/polycentric-client.ts
+++ packages/js-core/src/polycentric-client.ts
@@ -767,14 +767,6 @@ export class PolycentricClient {
     this.events.emitError(error);
   }
 
-  private bytesEqual(a: Uint8Array, b: Uint8Array): boolean {
-    if (a.length !== b.length) return false;
-    for (let i = 0; i < a.length; i++) {
-      if (a[i] !== b[i]) return false;
-    }
-    return true;
-  }
-
   get currentSystem(): Proto.PublicKey {
     return this.currentKeyPair!.publicKey;
   }
