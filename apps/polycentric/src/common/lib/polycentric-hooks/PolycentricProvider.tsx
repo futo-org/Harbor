23917fe71050dc7f4ccda78263565a6652862f9e
Brandon Hicks <brandonh@futo.org>
2026-06-08 16:02:35 -0500

    Fix server list initialization

--- apps/polycentric/src/common/lib/polycentric-hooks/PolycentricProvider.tsx
+++ apps/polycentric/src/common/lib/polycentric-hooks/PolycentricProvider.tsx
@@ -175,8 +175,6 @@ export function PolycentricProvider({
 
         if (cancelled) return;
 
-        c.servers = [DEFAULT_SERVER];
-
         const s = createPolycentricStore(c);
         await s.getState().refreshIdentities();
 
