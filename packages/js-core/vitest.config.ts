4192fb0b05ba4ba2c63245d936fc6afa0d38873c
Brandon Hicks <brandonh@futo.org>
2026-06-12 10:05:15 -0500

    Prepare js-core tests for CI

--- packages/js-core/vitest.config.ts
+++ packages/js-core/vitest.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vitest/config';
+import path from 'path';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@polycentric/rs-core-uniffi-web/generated': path.resolve(
+        __dirname,
+        'src/__mocks__/rs-core-uniffi-web-generated.ts',
+      ),
+    },
+  },
   test: {
     environment: 'node',
     include: ['src/**/*.test.ts'],
