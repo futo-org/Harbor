7abaee1d819421b214c6004c4e3b6e697a52a7f5
Paul Stach <paul@Paul-debian.paul-laptop>
2026-08-07 11:56:29 -0500

    Fixed a compile time error

--- services/server/src/service/notifications/worker.rs
+++ services/server/src/service/notifications/worker.rs
@@ -540,6 +540,7 @@ mod tests {
             event_key: Some(target.clone()),
             emoji: None,
             positive: true,
+            attributed_to: None,
         }));
 
         let notification = single(build_notifications("alice", &c))
