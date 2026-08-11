2c470c34a81cf9f5914bc0093c1ac1f12d6e7c89
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2026-08-11 10:45:50 +0200

    Fix Clippy lint

--- packages/rs-common/src/lib.rs
+++ packages/rs-common/src/lib.rs
@@ -6,10 +6,7 @@ pub mod platform;
 pub mod signing;
 
 fn encode_hex(bytes: &[u8]) -> String {
-    const HEX_CHARS: [u8; 16] = [
-        b'0', b'1', b'2', b'3', b'4', b'5', b'6', b'7', b'8', b'9', b'a', b'b', b'c', b'd', b'e',
-        b'f',
-    ];
+    const HEX_CHARS: [u8; 16] = *b"0123456789abcdef";
     let mut buf = String::with_capacity(2 * bytes.len());
     for byte in bytes {
         buf.push(HEX_CHARS[usize::from(byte >> 4)] as char);
