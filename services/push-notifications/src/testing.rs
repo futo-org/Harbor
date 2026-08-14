9cd7a028556c311953d8e9221e69d7a75cb5b975
Paul Stach <paul@Paul-debian.paul-laptop>
2026-08-14 10:08:21 -0400

    Added a missing mock method

--- services/push-notifications/src/testing.rs
+++ services/push-notifications/src/testing.rs
@@ -7,7 +7,8 @@ use crate::polycentric::PolycentricClient;
 use polycentric_common::models::{
     collections,
     protos_v2::{
-        Content, Event, EventBundle, EventKey, GetReactionsRequest, GetReactionsResponse, Identity,
+        Content, Event, EventBundle, EventKey, GetAttributedToReactionCountsRequest,
+        GetAttributedToReactionCountsResponse, GetReactionsRequest, GetReactionsResponse, Identity,
         KeyType, ListEventsRequest, ListEventsResponse, ListHeadsRequest, ListHeadsResponse, Post,
         PostReply, ProfileUpdate, PublicKey, PutEventsRequest, PutEventsResponse,
         SerializedContent, SignedEvent,
@@ -99,6 +100,13 @@ impl EventSyncService for MockEventSync {
         &self,
         _request: Request<GetReactionsRequest>,
     ) -> Result<Response<GetReactionsResponse>, Status> {
+        Err(Status::unimplemented("not needed for these tests"))
+    }
+
+    async fn get_attributed_to_reaction_counts(
+        &self,
+        _request: Request<GetAttributedToReactionCountsRequest>,
+    ) -> Result<Response<GetAttributedToReactionCountsResponse>, Status> {
         Err(Status::unimplemented("not needed for these tests"))
     }
 }
