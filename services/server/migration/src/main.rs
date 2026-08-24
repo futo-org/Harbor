72dea21f3aa1afbae4ebdec9a81e8bfb02b7450e
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2026-08-24 10:15:52 +0200

    Allow migrations to use HARBOR_DATABASE_URL env var

--- services/server/migration/src/main.rs
+++ services/server/migration/src/main.rs
@@ -1,6 +1,19 @@
 use sea_orm_migration::prelude::*;
+use std::env;
 
 #[tokio::main]
 async fn main() {
+    // The cli loads the .env file for us, but requires the DATABASE_URL env
+    // var, whic we don't set. To work around this load the .env file ourselves
+    // and then copy HARBOR_DATABASE_URL to DATABASE_URL so the migrator is
+    // happy.
+    common_dotenv::load(".env");
+    common_dotenv::load("../.env"); // So it can be run in the migration directory.
+    if let Err(_) = env::var("DATABASE_URL")
+        && let Ok(db_url) = env::var("HARBOR_DATABASE_URL")
+    {
+        unsafe { env::set_var("DATABASE_URL", db_url) }
+    }
+
     cli::run_cli(migration::Migrator).await;
 }
