4d6c40587424c06a8df397d08eeb9e46890060e6
Thomas de Zeeuw <thomas@futo.org>
2026-09-03 09:42:38 +0200

    Fix formatting

--- services/server/src/service/events/repository.rs
+++ services/server/src/service/events/repository.rs
@@ -414,7 +414,9 @@ impl Mutation {
             with.cte(cte);
         }
 
-        if let Some(reply) = post.reply.as_ref() && let Some(reply) = &reply.parent {
+        if let Some(reply) = post.reply.as_ref()
+            && let Some(reply) = &reply.parent
+        {
             // NOTE: only adding a reply to the parent, not for the root.
             let key = split_event_key(Some(reply.clone()), "reply")
                 .map_err(|err| DbErr::Custom(err.message().into()))?;
