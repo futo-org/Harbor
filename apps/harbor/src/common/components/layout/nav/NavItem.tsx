f83ccd57c6a86ddd7877b8371d299e24a6ca8f9a
Thomas de Zeeuw <thomas@futo.org>
2026-09-16 11:53:31 +0200

    Run formatter

--- apps/harbor/src/common/components/layout/nav/NavItem.tsx
+++ apps/harbor/src/common/components/layout/nav/NavItem.tsx
@@ -1,9 +1,4 @@
-import {
-  Link,
-  type LinkProps,
-  router,
-  usePathname,
-} from 'expo-router';
+import { Link, type LinkProps, router, usePathname } from 'expo-router';
 import { emitFocusedRefresh } from '@/src/common/lib/navigation/useFocusedRefresh';
 import {
   cloneElement,
