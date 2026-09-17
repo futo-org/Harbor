f028e2cad1434f784c2dd5c40ccd88380505f510
Thomas de Zeeuw <thomas@futo.org>
2026-09-17 14:54:50 +0000

    Support sorting by top when searching posts (#1560)
    Closes #225
    
    Changelog: feature
    Reviewed-on: https://code.futo.org/harbor/harbor/pulls/1560
    Reviewed-by: Brandon <brandonh@futo.org>

--- services/server/src/service/search/rpc/search_posts.rs
+++ services/server/src/service/search/rpc/search_posts.rs
@@ -27,13 +27,6 @@ pub async fn handle(
 ) -> Result<SearchPostsResponse, Status> {
     let sort_by = req.sort_by();
 
-    // TODO: implemented this. Alos remove the unreachable call from fetch.
-    if sort_by == SortPostsBy::Top {
-        return Err(Status::unimplemented(
-            "ordering by top is not implemented",
-        ));
-    }
-
     let common = rpc::Params::from_req_params(req.query, &req.page_params)?;
     let params = Params {
         common,
@@ -68,8 +61,9 @@ async fn fetch(
         |row| Marker {
             sorted_by: match params.sort_by {
                 SortPostsBy::Default => SortedPostsBy::Rank(row.search_rank),
-                // Checked in handle above.
-                SortPostsBy::Top => unimplemented!(),
+                SortPostsBy::Top => {
+                    SortedPostsBy::PositiveReactions(row.positive_reactions)
+                }
                 SortPostsBy::Latest => {
                     SortedPostsBy::Latest(row.event.created_at)
                 }
@@ -88,6 +82,9 @@ async fn fetch(
 pub enum SortedPostsBy {
     /// ts_rank rank returned by Postgres.
     Rank(f32),
+    /// Amount of positive reactions on the post.
+    PositiveReactions(i64),
+    /// Event creation timestamp.
     Latest(DateTimeWithTimeZone),
 }
 
@@ -95,6 +92,7 @@ impl SortedPostsBy {
     pub fn matches(&self, sort_by: SortPostsBy) -> bool {
         match self {
             SortedPostsBy::Rank(_) => sort_by == SortPostsBy::Default,
+            SortedPostsBy::PositiveReactions(_) => sort_by == SortPostsBy::Top,
             SortedPostsBy::Latest(_) => sort_by == SortPostsBy::Latest,
         }
     }
