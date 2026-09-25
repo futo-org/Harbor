6693d838077427a91d6ea54623f1c88921540130
Thomas de Zeeuw <thomas@futo.org>
2026-09-25 13:41:05 +0200

    Use Validate impl of Post in put_events

--- services/server/src/service/events/rpc/put_events.rs
+++ services/server/src/service/events/rpc/put_events.rs
@@ -2,7 +2,6 @@
 //! events pipeline.
 
 use std::collections::{HashMap, HashSet};
-use std::fmt;
 use std::time::Duration;
 
 use chrono::{DateTime, Utc};
@@ -22,11 +21,10 @@ use crate::service::context::ServiceContext;
 use crate::service::events::repository as EventsRepository;
 use crate::service::identity::repository::Query as IdentityRepository;
 use crate::service::identity::service::authorize_event_signer;
-use crate::service::proto::attributed_to::To;
 use crate::service::proto::content::ContentBody;
 use crate::service::proto::{
-    AttributedTo, Content, Delete, Event, EventBundle, Image, ImageSet, Link,
-    Post, PublicKey, PutEventError, PutEventsRequest, PutEventsResponse,
+    Content, Delete, Event, EventBundle, PublicKey, PutEventError,
+    PutEventsRequest, PutEventsResponse,
 };
 
 /// Ingest a batch of signed events. Each event is processed in
@@ -107,6 +105,14 @@ async fn process_event(
             tracing::debug!(error = %e, "put_events decode error");
             Status::invalid_argument("signed event bytes invalid")
         })?;
+    // TODO: validate:
+    // * event.key.collection -> match ContentBody variant
+    // * event.key.sequence
+    // * event.key.identity?
+    // * event.identity_sequence
+    // * event.vector_clock
+    // * event.previous_signature
+    // * event.previous_root
     Validate::validate(&event)
         .map_err(|err| Status::invalid_argument(format!("event {err}")))?;
     let collection = event.key.as_ref().map(|k| k.collection).unwrap_or(0);
@@ -317,92 +323,9 @@ fn validate_content(content: &Content, collection: i32) -> Result<(), Status> {
     match content_body {
         ContentBody::Post(post) => {
             check_collection(collection, collections::FEED)?;
-            let Post {
-                text,
-                reply: _,
-                images,
-                quote: _,
-                links,
-                labels,
-                attributed_to,
-            } = post;
-            validate_string(text, "post text", Some(1), Some(2000))?;
-            validate_slice(
-                images,
-                "post image set",
-                None,
-                Some(10),
-                |image_set| {
-                    let ImageSet { images } = image_set;
-                    validate_slice(
-                        images,
-                        "post images",
-                        None,
-                        Some(10),
-                        |image| {
-                            let Image {
-                                blob,
-                                width: _,
-                                height: _,
-                            } = image;
-                            let Some(blob) = blob else {
-                                return Err(Status::invalid_argument(
-                                    "missing post image blob",
-                                ));
-                            };
-                            let Blob {
-                                digest,
-                                mime_type,
-                                size,
-                            } = blob;
-                            let Some(digest) = digest else {
-                                return Err(Status::invalid_argument(
-                                    "missing post image blob digest",
-                                ));
-                            };
-                            // TODO: validate digest.
-                            _ = digest;
-                            validate_string(
-                                mime_type,
-                                "post image blob mime type",
-                                Some(1),
-                                Some(100),
-                            )?;
-                            const MAX_SIZE: i64 = 100 * 1024 * 1024; // 100 MB
-                            validate_int(
-                                *size,
-                                "post image blob size",
-                                Some(1),
-                                Some(MAX_SIZE),
-                            )?;
-                            Ok(())
-                        },
-                    )
-                },
-            )?;
-            validate_slice(links, "post links", None, Some(10), |link| {
-                validate_link(link, "post link")
+            post.validate().map_err(|err| {
+                Status::invalid_argument(format!("event {err}"))
             })?;
-            validate_slice(labels, "post labels", None, Some(10), |label| {
-                validate_string(label, "post label", Some(1), Some(200))
-            })?;
-            validate_slice(
-                attributed_to,
-                "post attributed to",
-                None,
-                Some(10),
-                |attributed_to| {
-                    let AttributedTo { to } = attributed_to;
-                    match to {
-                        Some(To::Link(link)) => {
-                            validate_link(link, "post attributed to link")
-                        }
-                        None => Err(Status::invalid_argument(
-                            "missing post attributed to",
-                        )),
-                    }
-                },
-            )?;
 
             // TODO: needs db for validation of:
             // * reply.root & reply.parent events exists.
@@ -489,103 +412,6 @@ fn check_collection(collection: i32, expected: i32) -> Result<(), Status> {
             "event key collection invalid: expected '{expected}', got '{collection}'"
         )))
     }
-}
-
-fn validate_link(link: &Link, prefix: &'static str) -> Result<(), Status> {
-    let Link {
-        title,
-        description,
-        image,
-        url,
-    } = link;
-    validate_string(title, format_args!("{prefix} title"), Some(1), Some(100))?;
-    validate_string(
-        description,
-        format_args!("{prefix} description"),
-        Some(1),
-        Some(200),
-    )?;
-    validate_string(image, format_args!("{prefix} image"), Some(1), Some(200))?;
-    validate_string(url, format_args!("{prefix} url"), Some(1), Some(200))
-}
-
-fn validate_string(
-    input: &str,
-    name: impl fmt::Display,
-    min_len: Option<usize>,
-    max_len: Option<usize>,
-) -> Result<(), Status> {
-    if let Some(1) = min_len
-        && input.is_empty()
-    {
-        return Err(Status::invalid_argument(format!("{name} can't be empty")));
-    } else if let Some(min_len) = min_len
-        && input.len() < min_len
-    {
-        return Err(Status::invalid_argument(format!("{name} too short")));
-    }
-
-    if let Some(max_len) = max_len
-        && input.len() > max_len
-    {
-        return Err(Status::invalid_argument(format!("{name} too long")));
-    }
-
-    Ok(())
-}
-
-fn validate_int<Int>(
-    input: Int,
-    name: impl fmt::Display,
-    min_len: Option<Int>,
-    max_len: Option<Int>,
-) -> Result<(), Status>
-where
-    Int: Eq + Ord,
-{
-    if let Some(min_len) = min_len
-        && input < min_len
-    {
-        return Err(Status::invalid_argument(format!("{name} too small")));
-    }
-
-    if let Some(max_len) = max_len
-        && input > max_len
-    {
-        return Err(Status::invalid_argument(format!("{name} too large")));
-    }
-
-    Ok(())
-}
-
-fn validate_slice<T>(
-    input: &[T],
-    name: impl fmt::Display,
-    min_len: Option<usize>,
-    max_len: Option<usize>,
-    mut validate: impl FnMut(&T) -> Result<(), Status>,
-) -> Result<(), Status> {
-    if let Some(1) = min_len
-        && input.is_empty()
-    {
-        return Err(Status::invalid_argument(format!("{name} can't be empty")));
-    } else if let Some(min_len) = min_len
-        && input.len() < min_len
-    {
-        return Err(Status::invalid_argument(format!("{name} too short")));
-    }
-
-    if let Some(max_len) = max_len
-        && input.len() > max_len
-    {
-        return Err(Status::invalid_argument(format!("{name} too long")));
-    }
-
-    for item in input {
-        validate(item)?;
-    }
-
-    Ok(())
 }
 
 /// Check if the `identity` is authorised to perform its mutation.
